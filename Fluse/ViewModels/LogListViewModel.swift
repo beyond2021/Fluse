f9a9d30824ac13c8faed78593eda58889cbb083d
KEEVIN MITCHELL <keevinmitchell@KEEVINs-Mini-2.lan1>
2024-06-13 03:47:23 -0400

    Category selection error corrected commit

--- Fluse/ViewModels/LogListViewModel.swift
+++ Fluse/ViewModels/LogListViewModel.swift
@@ -12,23 +12,25 @@ import Observation
 @Observable
 
 class LogListViewModel {
-    let db = DatabaseManager.shared
-    var sortType: SortType = .date
-    var sortOrder: SortOrder = .descending
-    var selectedCategories = Set<Category>() // ENUM
     
-    //add or edit from sheet
-    //add
+    let db = DatabaseManager.shared
+    
+    var sortType = SortType.date
+    var sortOrder = SortOrder.descending
+    var selectedCategories = Set<Category>()
+    
     var isLogFormPresented = false
     var logToEdit: ExpenseLog?
+    
     
     var predicates: [QueryPredicate] {
         var predicates = [QueryPredicate]()
         if selectedCategories.count > 0 {
-            predicates.append(.whereField("category", isIn: Array(selectedCategories).map {$0.rawValue}))
-//            predicates.append(.whereField("category", isIn: [Category.food.rawValue]))
+            predicates.append(.whereField("category", isIn: Array(selectedCategories).map { $0.rawValue }))
         }
+        
         predicates.append(.order(by: sortType.rawValue, descending: sortOrder == .descending ? true : false))
         return predicates
     }
+    
 }
--- Fluse/Views/FilterCategoriesView.swift
+++ Fluse/Views/FilterCategoriesView.swift
@@ -8,66 +8,66 @@
 import SwiftUI
 
 struct FilterCategoriesView: View {
+    
     @Binding var selectedCategories: Set<Category>
     private let categories = Category.allCases
+    
     var body: some View {
         VStack {
-            ScrollView(.horizontal, showsIndicators: false) {
+            ScrollView(.horizontal) {
                 HStack(spacing: 8) {
                     ForEach(categories) { category in
                         FilterButtonView(category: category, isSelected: self.selectedCategories.contains(category), onTap: self.onTap)
-                        
                     }
                 }
                 .padding(.horizontal)
             }
+            
             if selectedCategories.count > 0 {
                 Button(role: .destructive) {
-                    self.selectedCategories
-                        .removeAll()
+                    self.selectedCategories.removeAll()
                 } label: {
-                    Text("Clear all filter selections (\(self.selectedCategories.count))")
+                    Text("Clear all filter selection (\(self.selectedCategories.count))")
                 }
             }
+            
         }
     }
+    
     func onTap(category: Category) {
-        /// same type as FilterButtonView
         if selectedCategories.contains(category) {
-            /// remove it by toggling it
-            selectedCategories.remove(category) // Set
+            selectedCategories.remove(category)
         } else {
-           /// Add
-            selectedCategories.insert(category) // Set
-            print(category.rawValue)
+            selectedCategories.insert(category)
         }
     }
 }
 
 struct FilterButtonView: View {
+    
     var category: Category
     var isSelected: Bool
-    var onTap: (Category) -> () // Handler, action passes a category returns Void
+    var onTap: (Category) -> ()
+    
     var body: some View {
         HStack(spacing: 4) {
             Text(category.rawValue.capitalized)
-                .fixedSize(horizontal: true, vertical: true)
+                .fixedSize(horizontal: true, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
         }
         .padding(.horizontal, 16)
         .padding(.vertical, 4)
         .background {
             RoundedRectangle(cornerRadius: 16)
-                .stroke(isSelected ? category.color : Color.gray, lineWidth:  1)
+                .stroke(isSelected ? category.color : Color.gray, lineWidth: 1)
                 .overlay {
-                    RoundedRectangle(cornerRadius: 16)
-                        .foregroundStyle(isSelected ? category.color : Color.clear)
+                    RoundedRectangle(cornerRadius: 16).foregroundColor(isSelected ? category.color : Color.clear)
                 }
         }
         .frame(height: 44)
         .onTapGesture {
             self.onTap(category)
         }
-        .foregroundStyle(isSelected ? .white : Color.primary)
+        .foregroundColor(isSelected ? .white : nil)
     }
 }
 
